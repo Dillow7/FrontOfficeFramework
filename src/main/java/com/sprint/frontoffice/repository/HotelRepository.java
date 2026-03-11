17192b59a01a7ffab621649007003b9c91fc0d48
Noa2107 <mihajarajaonarivelo21@gmail.com>
2026-03-11 08:50:20 -0200

    Add Correction API

--- src/main/java/com/sprint/frontoffice/repository/HotelRepository.java
+++ src/main/java/com/sprint/frontoffice/repository/HotelRepository.java
@@ -1,12 +1,4 @@
 package com.sprint.frontoffice.repository;
 
-import com.sprint.frontoffice.entity.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-/**
- * Repository pour l'entité Hotel
- */
-@Repository
-public interface HotelRepository extends JpaRepository<Hotel, Integer> {
+public interface HotelRepository {
 }
